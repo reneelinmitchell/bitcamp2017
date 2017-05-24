f8622045ffa6b2e6001de2043ab5045ed9179729
xiaosneeze56 <reneelinmitchell@gmail.com>
2017-05-24 16:34:36 +0800

    hi

---
+++ data/bitcampjson.py
@@ -0,0 +1,8 @@
+from responseFile import courtDict
+
+print(courtDict["dockets"])
+
+# store each value of the key in a variable
+# find the json files for each value
+# store them in variables
+# output to a file via linux
